aa131a3d351ee6ed030bad1e1af66c491c842c19
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-05-07 07:54:41 -0300

    laços de repetição

--- atividade2.js
+++ atividade2.js
@@ -1,9 +1,9 @@
-let tentativa = 3; //Contador sera iniciado com 5 pois na proposta é solicitado uma variavel para contar as tentativas
+let tentativa = 3; //Contador vai diminuindo 
 let numeroSecreto = Math.round(Math.random() * 10)
 let leitura
 let acerto = false
 
-while (tentativa > 0) { //enquanto a tentativa for maior que 5 o programa do loop irá acontecer
+while (tentativa > 0) { //o numero vai aparecer na tentativa enquanto o usuario faz
     leitura = parseInt(prompt("Tente adivinhar um numero entre 1 e 10"));
 
 
